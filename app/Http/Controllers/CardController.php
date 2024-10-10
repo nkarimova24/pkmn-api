1b4cf32979d11904fe297121e2434abb7c01f201
kw1c-KarimovaNargiz <giesje002@gmail.com>
2024-10-10 12:26:46 +0200

    added new functions in cardcontroller

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\Card;
 use App\Models\Set;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class CardController extends Controller
 {
@@ -15,8 +16,8 @@ class CardController extends Controller
 
     //cards from set
     public function cardsFromSet($setId){
-        $cards = Set::find($setId)->cards;
-        return response()->json($cards);
+        $cards = Set::with('cards.set')->find($setId)->cards;
+    return response()->json($cards);
     }
 
 
@@ -25,10 +26,6 @@ class CardController extends Controller
     {
        
         $query = $request->input('query');
-        
-        if (!$query) {
-            return response()->json(['error' => 'Search query is required'], 400);
-        }
 
         $results = Card::where('name', 'LIKE', '%' . $query . '%')->get();
 
@@ -39,4 +36,145 @@ class CardController extends Controller
         return response()->json($results);
     }
 
+
+
+    //filter by set
+    public function filterType(Request $request)
+    {
+        $type = $request->input('type');
+    
+        if (!$type) {
+            return response()->json(['error' => 'Type is required'], 400);
+        }
+    
+        if ($request->has('set_id')) {
+            $setId = $request->input('set_id');
+            $cards = Card::where('set_id', $setId)
+                ->where('types', 'LIKE', '%' . $type . '%')
+                ->get();
+        } else {
+            $cards = Card::where('types', 'LIKE', '%' . $type . '%')->get();
+        }
+    
+     
+    
+        return response()->json($cards);
+    }
+    
+    public function orderEvolutionBySets($setId) {
+       
+        //fetch cards from current
+        $set = Set::with('cards')->find($setId);
+
+        if (!$set) {
+            Log::info("No set found for ID: $setId");
+            return response()->json([]); 
+        }
+        
+        $cards = $set->cards;
+
+        if ($cards->isEmpty()) {
+            Log::info("No cards found for set ID: $setId");
+            return response()->json([]); 
+        }
+    
+        //map of cards
+        $cardMap = [];
+        foreach ($cards as $card) {
+            $cardMap[$card->name] = $card;
+        }
+    
+        //grouping cards by evo chain
+        $evolutionChains = [];
+    
+        foreach ($cards as $card) {
+            //(Basic, Stage 1, Stage 2)
+            $evolutionStage = 'Basic'; 
+            if (in_array('Stage 2', $card->subtypes)) {
+                $evolutionStage = 'Stage 2';
+            } elseif (in_array('Stage 1', $card->subtypes)) {
+                $evolutionStage = 'Stage 1';
+            }
+            //starting an evo chain from Basic
+            if ($evolutionStage === 'Basic' || empty($card->evolves_from)) {
+                $evolutionChains[$card->name] = [$card];
+            } else {
+                //if stage 1 or stage 2, add to the corresponding chain based on 'evolves_from'
+                if (isset($evolutionChains[$card->evolves_from])) {
+                   
+                    $evolutionChains[$card->evolves_from][] = $card;
+
+                } elseif (isset($cardMap[$card->evolves_from])) {
+                  
+                    $evolutionChains[$card->evolves_from] = [$cardMap[$card->evolves_from], $card];
+                }
+            }
+        }
+    
+        //sorted list
+        $sortedEvo = [];
+        foreach ($evolutionChains as $chain) {
+            //sorting by from basic -> s1 -> s2
+            usort($chain, function($a, $b) {
+                return $this->getEvolutionOrder($a) - $this->getEvolutionOrder($b);
+            });
+    
+            // Add the sorted chain to the final result
+            foreach ($chain as $card) {
+                $sortedEvo[] = $card;
+            }
+        }
+    
+        //cards that do not have a basic as previous, so cards as EX and such
+        foreach ($cards as $card) {
+            if (empty($card->evolves_from) && !array_key_exists($card->name, $evolutionChains)) {
+                $sortedEvo[] = $card; 
+            }
+        }
+    
+        return response()->json($sortedEvo);
+    }
+    
+    private function getEvolutionOrder($card) {
+        if (in_array('Stage 2', $card->subtypes)) {
+            return 3; 
+        } elseif (in_array('Stage 1', $card->subtypes)) {
+            return 2; 
+        }
+        return 1;
+    }
+    
+    public function subTypes($setId)
+{
+    $set = Set::with('cards')->find($setId);
+
+    if (!$set) {
+        return response()->json(['message' => 'Set not found'], 404);
+    }
+
+    $cards = $set->cards;
+
+    if ($cards->isEmpty()) {
+        return response()->json(['message' => 'No cards found in this set'], 404);
+    }
+
+    //subtypes to exclude (evolutions)
+    $excludedSubtypes = [
+        "Basic", "Stage 1", "Stage 2", "Stage 3",
+    ];
+
+    $subtypeArray = [];
+    // checking if subtype exists and if is an array
+    foreach ($cards as $card) {
+        if (isset($card->subtypes) && is_array($card->subtypes)) {
+            foreach ($card->subtypes as $subtype) {
+                if (!in_array($subtype, $excludedSubtypes) && !in_array($subtype, $subtypeArray)) {
+                    $subtypeArray[] = $subtype;
+                }
+            }
+        }
+    }
+
+    return response()->json($subtypeArray);
+}
 }
--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,20 @@ use App\Http\Controllers\CardController;
 use App\Http\Controllers\SeriesController;
 ini_set('memory_limit', '-1');
 
-
-
-
 //series and their sets in one :)
 Route::get('/series', [SeriesController::class, 'index']  );
-   
 
 //card to according set
 Route::get('/sets/{setId}/cards', [CardController::class, 'cardsFromSet']);
 
-
 //searching for specific card
 Route::get('/search', [CardController::class, 'search']);
+
+//types && subtypes
+Route::get('/cards/filter', [CardController::class, 'filterType']);
+//
+Route::get('/subtypes/{setId}', [CardController::class, 'subTypes']);
+
+//evolutionchains per set
+Route::get('/cards/set/{setId}/sorted', [CardController::class, 'orderEvolutionBySets']);
+
